6132f929957ce601f80394754342de1822001eaa
512642 <72399788+512642@users.noreply.github.com>
2022-02-16 22:23:51 +0000

    sdf

--- MarioMobile/Assets/Scripts/PlayerMechanics.cs
+++ MarioMobile/Assets/Scripts/PlayerMechanics.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerMechanics : MonoBehaviour
 {
-    [Header ("Player Variables")]
-    [SerializeField] float walk;
-    [SerializeField] float run = 2000;
-    [SerializeField] float saveWalk = 1000;
-    [SerializeField] float jump = 1000;
-    [SerializeField] bool jumpButtonPressed = false;
-    [SerializeField] bool sprintButtonPressed = false;
+    [Header("Player Variables")]
+    [SerializeField]    float x;
+    [SerializeField]    float speed;
+    [SerializeField]    float walk = 1000;
+    [SerializeField]    float run = 2000;
+    [SerializeField]    float jump = 10;
+    [SerializeField]    bool jumpButtonPressed = false;
+    [SerializeField]    bool sprintButtonPressed = false;
 
     [Header ("Physics")]
     [SerializeField]  Rigidbody2D rb;
@@ -18,18 +19,49 @@ public class PlayerMechanics : MonoBehaviour
     [Header("Scripts")]
     [SerializeField] ButtonsScript jumpButtonScipt;
     [SerializeField] ButtonsScript sprintButtonScipt;
+    [SerializeField] Joystick joystick;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = walk;
+        bool sprintButtonPressed = sprintButtonScipt.sprintPresseed;
+        bool jumpButtonPressed = jumpButtonScipt.jumpPressed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Walking();
+        Sprinting();
+        Jumping();
+    }
+
+    public void Walking()
+    {
+        x = joystick.Horizontal;
+        rb.velocity = new Vector2(x * speed, 0) * Time.deltaTime;
+    }
+
+    public void Sprinting()
+    {
+        if(sprintButtonPressed == true)
+        {
+            speed = run;
+        }
+        else
+        {
+            speed = walk;
+        }
+    }
+
+    public void Jumping()
+    {
+        if(jumpButtonPressed == true)
+        {
+            rb.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+        }        
     }
 }
